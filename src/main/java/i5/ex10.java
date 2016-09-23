b6174c01ddc589e0ab52040c3bcc2970e856128d
Vadim Tishenko <vad.tishenko@gmail.com>
2016-09-23 21:56:48 +0300

    ex10 & ex 11

---
+++ src/main/java/i5/ex10.java
@@ -0,0 +1,18 @@
+package i5;
+
+import java.time.*;
+import java.time.temporal.Temporal;
+
+/**
+ * 10. Your flight from Los Angeles to Frankfurt leaves at 3:05 pm local time and
+ * takes 10 hours and 50 minutes. When does it arrive? Write a program that
+ * can handle calculations like this.
+ */
+public class ex10 {
+    public static void main(String[] args) {
+        ZonedDateTime depLosAngeles = LocalDateTime.parse("2007-12-03T15:25:00").atZone(ZoneId.of("America/Los_Angeles"));
+        Duration dur = Duration.parse("PT10H50M");
+        ZonedDateTime ff = depLosAngeles.plus(dur).withZoneSameInstant(ZoneId.of("Europe/Berlin"));
+        System.out.println("departure: " + depLosAngeles + "\n      dur: " + dur + "\n   arrive: " + ff);
+    }
+}
---
+++ src/main/java/i5/ex11.java
@@ -0,0 +1,17 @@
+package i5;
+
+import java.time.*;
+
+/**
+ 11. Your return flight leaves Frankfurt at 14:05 and arrives in Los Angeles at
+ 16:40. How long is the flight? Write a program that can handle calculations
+ like this.
+ */
+public class ex11 {
+    public static void main(String[] args) {
+        ZonedDateTime depFranfurkt = LocalDateTime.parse("2007-12-03T14:05").atZone(ZoneId.of("Europe/Berlin"));
+        ZonedDateTime arrLosAngeles = LocalDateTime.parse("2007-12-03T16:40").atZone(ZoneId.of("America/Los_Angeles"));
+        Duration dur = Duration.between(depFranfurkt, arrLosAngeles);
+        System.out.println("departure: "+depFranfurkt+"\n      dur: "+dur+"\n   arrive: "+arrLosAngeles);
+    }
+}
