af4526a4a5828c181a2298478ff667c78f887918
Vadim Tishenko <vad.tishenko@gmail.com>
2016-09-26 22:25:59 +0300

    ex 8 & 9

--- src/main/java/i5/ex8_1.java
+++ src/main/java/i5/ex8_1.java
@@ -10,14 +10,15 @@ import java.time.ZonedDateTime;
  * time instant, turning ZoneId.getAvailableIds into a stream and using stream
  * operations.
  */
-public class ex8 {
+public class ex8_1 {
     public static void main(String[] args) {
         Instant now = Instant.now();
         ZonedDateTime utc = now.atZone(ZoneOffset.UTC);
         System.out.println(utc);
         ZoneId.getAvailableZoneIds().stream().sorted().forEach((x) -> {
-            ZonedDateTime d = utc.withZoneSameInstant(ZoneId.of(x));
-                System.out.println(d.getOffset());
+            ZonedDateTime i = utc.withZoneSameInstant(ZoneId.of(x));
+            ZonedDateTime l = utc.withZoneSameLocal(ZoneId.of(x));
+                System.out.printf("i:%-55s %7s  l:%-55s %7s\n",i,i.getOffset(),l,l.getOffset());
         });
     }
 }
--- src/main/java/i5/ex9.java
+++ src/main/java/i5/ex9.java
@@ -1,25 +1,25 @@
 package i5;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  9. Again using stream operations, find all time zones whose offsets aren’t full
  hours.
  */
 public class ex9 {
-    // TODO: 23.09.2016
     public static void main(String[] args) {
-        /*Instant now = Instant.now();
+        Instant now = Instant.now();
         ZonedDateTime utc = now.atZone(ZoneOffset.UTC);
         System.out.println(utc);
         ZoneId.getAvailableZoneIds().stream().sorted().forEach((x) -> {
-            System.out.println(x);
-            //ZoneId.of(x).
+            ZonedDateTime d = utc.withZoneSameLocal(ZoneId.of(x));
+            Duration dur = Duration.between(utc, d);
+            long sec = dur.get(SECONDS);
+            if(sec % (60*60)!=0) {
+                System.out.println(d.getOffset());
+            }
         });
-        // TODO: 22.09.2016  Obtain the offsets
-        //  ZoneOffset.UTC;*/
     }
 }
