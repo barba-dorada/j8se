8b8b35fbcc4d2a1776cc4aeccdd995a2a2d07fee
tishenko <vad.tishenko@gmail.com>
2016-10-13 13:33:41 +0300

    ex12

--- src/main/java/i5/ex12.java
+++ src/main/java/i5/ex12.java
@@ -1,9 +1,8 @@
 package i5;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
+import java.time.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  12. Write a program that solves the problem described at the beginning of
@@ -13,14 +12,16 @@ import java.time.ZonedDateTime;
  */
 public class ex12 {
     public static void main(String[] args) {
-       /* Instant now = Instant.now();
-        ZonedDateTime utc = now.atZone(ZoneOffset.UTC);
-        System.out.println(utc);
-        ZoneId.getAvailableZoneIds().stream().sorted().forEach((x) -> {
-            System.out.println(x);
-            //ZoneId.of(x).
-        });*/
-        // TODO: 22.09.2016  Obtain the offsets
-        //  ZoneOffset.UTC;
+        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime n = ZonedDateTime.of(now, ZoneId.of("Europe/Moscow")).minusMinutes(10);
+
+        String[] tZones = {"Asia/Yekaterinburg", "Europe/London", "Europe/Paris", "Europe/Moscow"};
+        Map<String, ZonedDateTime> map = new HashMap<>();
+        for (String tZone : tZones) {
+            map.put(tZone, ZonedDateTime.of(now, ZoneId.of(tZone)));
+        }
+        map.values().stream().sorted()
+                .filter(i -> i.isAfter(n) && i.isBefore(n.plusMinutes(90)))
+                .forEach(i -> System.out.println(i));
     }
 }
