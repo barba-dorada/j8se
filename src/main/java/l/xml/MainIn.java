62202037d6261c343271f7cd7ef79e485d03d267
Vadim Tishenko <vad.tishenko@gmail.com>
2017-01-23 22:32:05 +0300

    MainIn ok

--- src/main/java/l/xml/MainIn.java
+++ src/main/java/l/xml/MainIn.java
@@ -2,18 +2,16 @@ package l.xml;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.math.BigDecimal;
 
 public class MainIn {
     public static void main(String[] args) throws JAXBException {
-        JAXBContext jc=JAXBContext.newInstance(Courses.class);
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
-        File inFile = new File("./build/resources/main/l/xml/test.xml");
-        Courses res = (Courses) unmarshaller.unmarshal(inFile);
-
+        File file = new File( "build/resources/main/l/xml/test.xml" );
+        JAXBContext jaxbContext = JAXBContext.newInstance( Training.class );
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        Training training = (Training)jaxbUnmarshaller.unmarshal( file );
+        System.out.println( training );
     }
 
 }
