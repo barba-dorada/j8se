fa2426e227c609d1cef5e55a9d1f21ef9c74e99b
Vadim Tishenko <vad.tishenko@gmail.com>
2017-02-13 01:07:09 +0300

    wsimport refactoring

--- cbr_gismeteo_ws/src/gismeteogen.cmd
+++ cbr_gismeteo_ws/src/gismeteogen.cmd
@@ -1,5 +1,5 @@
 set wsi="%java_home%\bin\wsimport"
-set param=-extension -keep -XadditionalHeaders -s main/java -d dest
+set param=-extension -keep -XadditionalHeaders -s main/java -Xnocompile
 %wsi% %param% -p ru.cwl.ws.gismeteo.registration http://ws.gismeteo.ru/Registration/Register.asmx?WSDL
 %wsi% %param% -p ru.cwl.ws.gismeteo.locations http://ws.gismeteo.ru/Locations/Locations.asmx?WSDL
 %wsi% %param% -p ru.cwl.ws.gismeteo.weather http://ws.gismeteo.ru/Weather/Weather.asmx?WSDL
